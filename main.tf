a08045a1cd5466fcd012c9fe93a7ce954e36cc92
Heyman <35301662+The-Heyman@users.noreply.github.com>
2021-02-10 11:32:55 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-2"
+}
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc"
+  }
+}
